3024797f99e0e9fa9723a92dea4fc432108485cb
Bogdan Ion BUESCU <bbuescu@PENTALOG.com>
2018-10-05 10:32:16 +0300

    production config with pm2

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,43 @@ module.exports = {
       path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    app: ['./lib/renderers/dom.js'],
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node-modules/, use: 'babel-loader' }]
+    rules: [{
+      test: /\.js$/, exclude: /node-modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env', 'stage-2']
+        }
+      }
+    }]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          chunks: 'all',
+        }
+      }
+    }
   }
+  
 };
